63b1065a27330e82b0879dc6179a797c6ea939a1
Derek Kastner <dkastner@gmail.com>
2009-09-24 23:11:08 -0400

    Fixed test paths, removed some invalid cache testing

--- test/AllTests.php
+++ test/AllTests.php
@@ -1,24 +1,14 @@
 <?php
 require_once dirname(__FILE__).'/test_helper.php';
 require_once 'PHPUnit/Framework.php';
-require_once dirname(__FILE__).'/cdm/lib/Search/SearchFromParamsTest.php';
-require_once dirname(__FILE__).'/cdm/lib/Search/SearchFromParamStringTest.php';
-require_once dirname(__FILE__).'/cdm/lib/Search/SearchGenerateContentTypeSearchStringTest.php';
-require_once dirname(__FILE__).'/cdm/lib/Search/SearchGenerateSearchStringTest.php';
-require_once dirname(__FILE__).'/cdm/lib/Search/SearchTermsTest.php';
-require_once dirname(__FILE__).'/cdm/lib/Search/SearchTermSearchStringTest.php';
-require_once dirname(__FILE__).'/cdm/lib/SearchStatus/SearchStatusInitialize.php';
+// require_once dirname(__FILE__).'/theme/lib/cache/cache_test.php';
+require_once dirname(__FILE__).'/theme/lib/content_dm_test.php';
  
 class AllTests {
     public static function suite() {
         $suite = new PHPUnit_Framework_TestSuite;
-        $suite->addTestSuite('SearchFromParamsTest');
-        $suite->addTestSuite('SearchFromParamStringTest');
-        $suite->addTestSuite('SearchGenerateContentTypeSearchStringTest');
-        $suite->addTestSuite('SearchGenerateSearchStringTest');
-        $suite->addTestSuite('SearchTermsTest');
-        $suite->addTestSuite('SearchTermSearchStringTest');
-        $suite->addTestSuite('SearchStatusInitialize');
+        // $suite->addTestSuite('CacheTest');
+        $suite->addTestSuite('ContentDMTest');
 
         return $suite;
     }
