ea64371b769ec450f8aa2594fdbd36ecc89c5a6a
Archives <markharvey@mark-harveys-computer-3.local>
2009-11-23 11:33:20 -0500

    Added Pagination class to handle pagination

--- plugins/pagination/pagination.php
+++ plugins/pagination/pagination.php
@@ -45,7 +45,6 @@ if (!defined('WP_PLUGIN_URL')) {
 	define('WP_PLUGIN_URL', WP_CONTENT_URL.'/plugins');
 }
 
-
 // Create Text Domain For Translations
 add_action('init', 'pagination_textdomain');
 function pagination_textdomain() {
@@ -62,63 +61,6 @@ add_action('admin_menu', 'pagination_menu');
 function pagination_menu() {
 	if (function_exists('add_options_page')) {
 		add_options_page(__('Pagination', 'pagination'), __('Pagination', 'pagination'), 'manage_options', 'pagination/pagination-options.php') ;
-	}
-}
-
-function paginate() {
-	global $wpdb, $wp_query, $pagination;		// mmm global variables...
-	$pagination = array();
-	if (!is_single()) {
-		$request = $wp_query->request;
-		$posts_per_page = intval(get_query_var('posts_per_page'));
-		$current_page = intval(get_query_var('paged'));
-		$pagination_options = get_option('pagination_options');
-		$numposts = $wp_query->found_posts;
-		$max_page = $wp_query->max_num_pages;
-		if(empty($current_page) || $current_page == 0) {
-			$current_page = 1;
-		}
-		$pages_to_show = intval($pagination_options['num_pages']);
-		$pages_to_show_minus_1 = $pages_to_show-1;
-		$half_page_start = floor($pages_to_show_minus_1/2);
-		$half_page_end = ceil($pages_to_show_minus_1/2);
-		$start_page = $current_page - $half_page_start;
-		if($start_page <= 0) {
-			$start_page = 1;
-		}
-		$end_page = $current_page + $half_page_end;
-		if(($end_page - $start_page) != $pages_to_show_minus_1) {
-			$end_page = $start_page + $pages_to_show_minus_1;
-		}
-		if($end_page > $max_page) {
-			$start_page = $max_page - $pages_to_show_minus_1;
-			$end_page = $max_page;
-		}
-		if($start_page <= 0) {
-			$start_page = 1;
-		}
-		
-		$pagination['first_page'] = FALSE;
-		$pagination['last_page'] = FALSE;
-		$pagination['max_pages'] = $max_page;
-		$pagination['current_page'] = $current_page;
-		$pagination['display_num_pages'] = $pages_to_show;
-		
-		$pagination['pageset'] = array();
-		
-		if ($start_page >= 2 && $pages_to_show < $max_page) {
-			$pagination['first_page'] = clean_url(get_pagenum_link());
-		}
-		for($i = $start_page; $i  <= $end_page; $i++) {
-			if($i == $current_page) {
-				$pagination['pageset'][$i] = FALSE;
-			} else {
-				$pagination['pageset'][$i] = get_pagenum_link($i);
-			}
-		}
-		if ($end_page < $max_page) {
-			$pagination['last_page'] = get_pagenum_link($max_page);
-		}
 	}
 }
 
