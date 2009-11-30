a0650a2c4b37cc2716ec8106d492814a55d6a5ca
Archives <markharvey@mark-harveys-computer-3.local>
2009-11-30 14:02:55 -0500

    Need to variablize

--- theme/include/partials/search.php
+++ theme/include/partials/search.php
@@ -1,5 +1,5 @@
 <div class="search">
-  <form id="global-search" action="<?= home_url; ?>" method="get" >
+  <form id="global-search" action="<?= $home_url; ?>" method="get" >
     <label for="search-top" class="hidden">Seek: </label>
     <input type="text" name="s" id="s" value=" " />
     <label for="search-button" class="hidden">Search </label>
