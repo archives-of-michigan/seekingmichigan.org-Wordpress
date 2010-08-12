43723a12e984cf29bc1047b7fc217f284ecfd136
Derek Kastner <dkastner@gmail.com>
2010-08-11 22:00:33 -0400

    CivilWarCalendar now inherits from HttpClient

--- theme/lib/civil_war_calendar.php
+++ theme/lib/civil_war_calendar.php
@@ -1,14 +1,7 @@
 <?php
-set_include_path(get_include_path() . PATH_SEPARATOR . dirname(__FILE__));
-require_once('Zend/Cache.php');
-require_once('Zend/Http/Client.php');
-
-class CivilWarCalendar {
+class CivilWarCalendar extends HttpClient {
   private $_year;
   private $_month;
-  private $_frontend_options = array('lifetime' => 300, 'automatic_serialization' => true);
-  private $_backend_options = array('cache_dir' => '/tmp/');
-  private $_cache;
 
   function __construct($year = NULL, $month = NULL) {
     $this->_year = $year or date('%Y');
@@ -16,20 +9,7 @@ class CivilWarCalendar {
   }
 
   public function display() {
-    $response = $this->cache()->load('civil_war_calendar');
-    if(!$response || !$response->isSuccessful()) {
-      $url = "http://seeking-mi-civil-war-events.heroku.com/calendars/$this->_year/$this->_month";
-      $http = new Zend_Http_Client($url, array('timeout' => 30));
-      $response = $http->request();
-      $this->cache()->save($response, 'civil_war_calendar');
-    }
-    return $response->getBody();
-  }
-
-  private function cache() {
-    $this->_cache = $this->_cache ? 
-      $this->_cache :
-      Zend_Cache::factory('Core', 'File', $this->_frontend_options, $this->_backend_options);
-    return $this->_cache;
+    return $this->http_fetch('civil-war-calendar',
+      "http://seeking-mi-civil-war-events.heroku.com/calendars/$this->_year/$this->_month");
   }
 }
---
+++ theme/lib/http_client.php
@@ -0,0 +1,27 @@
+<?php
+set_include_path(get_include_path() . PATH_SEPARATOR . dirname(__FILE__));
+require_once('Zend/Cache.php');
+require_once('Zend/Http/Client.php');
+
+class HttpClient {
+  private $_frontend_options = array('lifetime' => 300, 'automatic_serialization' => true);
+  private $_backend_options = array('cache_dir' => '/tmp/');
+  private $_cache;
+
+  protected function http_fetch($cache_key, $url) {
+    $response = $this->cache()->load($cache_key);
+    if(!$response || !$response->isSuccessful()) {
+      $http = new Zend_Http_Client($url, array('timeout' => 30));
+      $response = $http->request();
+      $this->cache()->save($response, $cache_key);
+    }
+    return $response->getBody();
+  }
+
+  private function cache() {
+    $this->_cache = $this->_cache ? 
+      $this->_cache :
+      Zend_Cache::factory('Core', 'File', $this->_frontend_options, $this->_backend_options);
+    return $this->_cache;
+  }
+}
