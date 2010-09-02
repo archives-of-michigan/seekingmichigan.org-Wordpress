778c2aff2ac5bdd0412d0b4e8f7ca7b44aa5eff2
Derek Kastner <dkastner@gmail.com>
2010-09-01 21:26:31 -0400

    realized place links

--- theme/category_teach.php
+++ theme/category_teach.php
@@ -30,17 +30,17 @@ if($_GET['all'] == 'true') {
         <div class="wrapper">
           <div class="michigan-map">
             <ul class="maploc">
-              <li class="maploc1"><a href="#" title="Fort Wilkins"> Fort Wilkins</a> <strong>Fort Wilkins</strong></li>
-              <li class="maploc2"><a href="#" title="Iron Industry Museum"> Iron Industry Museum</a> <strong>Iron Industry Museum</strong></li>
-              <li class="maploc3"><a href="#" title="Father Marquette Memorial"> Father Marquette Memorial</a> <strong>Father Marquette Memorial</strong></li>
-              <li class="maploc4"><a href="#" title="Fayette Historic Townsite"> Fayette Historic Townsite</a> <strong>Fayette Historic Townsite</strong></li>
-              <li class="maploc5"><a href="#" title="Thunder Bay National Marine Sanctuary"> Thunder Bay National Marine Sanctuary</a> <strong>Thunder Bay National Marine Sanctuary</strong></li>
-              <li class="maploc6"><a href="#" title="Hartwick Pines and CCC Museum"> Hartwick Pines and CCC Museum</a> <strong>Hartwick Pines and CCC Museum</strong></li>
-              <li class="maploc7"><a href="#" title="Tawas Point Historic Lighthouse"> Tawas Point Historic Lighthouse</a> <strong>Tawas Point Historic Lighthouse</strong></li>
-              <li class="maploc8"><a href="#" title="Sanilac Petroglyphs"> Sanilac Petroglyphs</a> <strong>Sanilac Petroglyphs</strong></li>
-              <li class="maploc9"><a href="#" title="Michigan Historical Museum"> Michigan Historical Museum</a> <strong>Michigan Historical Museum</strong></li>
-              <li class="maploc10"><a href="#" title="Walker Tavern"> Walker Tavern</a> <strong>Walker Tavern</strong></li>
-              <li class="maploc11"><a href="#" title="Mann House"> Mann House</a> <strong>Mann House</strong></li>
+              <li class="maploc1"><a href="/fort-wilkins" title="Fort Wilkins"> Fort Wilkins</a> <strong>Fort Wilkins</strong></li>
+              <li class="maploc2"><a href="/iron-industry-museum" title="Iron Industry Museum"> Iron Industry Museum</a> <strong>Iron Industry Museum</strong></li>
+              <li class="maploc3"><a href="/father-marquette-memorial" title="Father Marquette Memorial"> Father Marquette Memorial</a> <strong>Father Marquette Memorial</strong></li>
+              <li class="maploc4"><a href="/fayette-historic-townsite" title="Fayette Historic Townsite"> Fayette Historic Townsite</a> <strong>Fayette Historic Townsite</strong></li>
+              <li class="maploc5"><a href="/thunder-bay-national-marine-sanctuary" title="Thunder Bay National Marine Sanctuary"> Thunder Bay National Marine Sanctuary</a> <strong>Thunder Bay National Marine Sanctuary</strong></li>
+              <li class="maploc6"><a href="/hartwick-pines-and-ccc-museum" title="Hartwick Pines and CCC Museum"> Hartwick Pines and CCC Museum</a> <strong>Hartwick Pines and CCC Museum</strong></li>
+              <li class="maploc7"><a href="/tawas-point-historic-lighthouse" title="Tawas Point Historic Lighthouse"> Tawas Point Historic Lighthouse</a> <strong>Tawas Point Historic Lighthouse</strong></li>
+              <li class="maploc8"><a href="/sanilac-petroglyphs" title="Sanilac Petroglyphs"> Sanilac Petroglyphs</a> <strong>Sanilac Petroglyphs</strong></li>
+              <li class="maploc9"><a href="/michigan-historical-museum" title="Michigan Historical Museum"> Michigan Historical Museum</a> <strong>Michigan Historical Museum</strong></li>
+              <li class="maploc10"><a href="/walker-tavern" title="Walker Tavern"> Walker Tavern</a> <strong>Walker Tavern</strong></li>
+              <li class="maploc11"><a href="/mann-house" title="Mann House"> Mann House</a> <strong>Mann House</strong></li>
             </ul>
         </div>
         <h2 class="aroundthe"><a href="/events/Around the State">Around the State</a></h2>
