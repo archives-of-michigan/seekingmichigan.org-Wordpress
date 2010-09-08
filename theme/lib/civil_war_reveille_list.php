53e5e4545e4dfd67790f8266bf716cc21dc865a1
Derek Kastner <dkastner@gmail.com>
2010-09-07 20:22:01 -0400

    Limit reveille list to 2 items

--- theme/lib/civil_war_reveille_list.php
+++ theme/lib/civil_war_reveille_list.php
@@ -1,9 +1,9 @@
 <?php
 
 class CivilWarReveilleList {
-  public static function grouped_by_date() {
+  public static function grouped_by_date($limit = 2) {
     $reveille_dates = array();
-    foreach(get_posts('category_name=reveille&numberposts=5') as $post) {
+    foreach(get_posts("category_name=reveille&numberposts=$limit") as $post) {
       $date = new DateTime($post->post_date);
       $date_string = $date->format('F j, Y');
       if(!$reveille_dates[$date_string]) {
