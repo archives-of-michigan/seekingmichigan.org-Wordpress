1409b3bb00d0f1faabbead66c532c151f19bfbed
Derek Kastner <dkastner@gmail.com>
2010-11-12 22:20:13 -0500

    Added youtube link

--- theme/include/partials/civil_war_videos.php
+++ theme/include/partials/civil_war_videos.php
@@ -1,11 +1,11 @@
 <h2 class="videos"><a href="#">Video</a></h2>
 <ul class="youtube">
-  <li><a href="#"><img src="/images/video-thumb1.jpg" height="78px" width="126px" style="background-color: #aaa" /></a></li>
-  <li><a href="#"><img src="/images/video-thumb2.jpg" height="78px" width="126px" style="background-color: #aaa" /></a></li>
+  <li><a href="http://www.youtube.com/user/MICivilWar"><img src="/images/video-thumb1.jpg" height="78px" width="126px" style="background-color: #aaa" /></a></li>
+  <li><a href="http://www.youtube.com/user/MICivilWar"><img src="/images/video-thumb2.jpg" height="78px" width="126px" style="background-color: #aaa" /></a></li>
 </ul>
 <p class="more">
-  <a href="#" class="logo-link">
+  <a href="http://www.youtube.com/user/MICivilWar" class="logo-link">
     <img src="/images/youtube-logo-color.gif" alt="youtube-logo" />
   </a>
-  <a href="#" class="addyours">Add Yours</a> 
+  <a href="http://www.youtube.com/user/MICivilWar" class="addyours">Add Yours</a> 
 </p>
