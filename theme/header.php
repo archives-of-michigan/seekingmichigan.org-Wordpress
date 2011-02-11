c9cc23206b32d3f12ccfb9875204124eda3d21e4
JillArnold <arnoldjil@gmail.com>
2011-02-11 14:17:51 -0500

    Updated Google Analytics loading code in attempt to fix bug with Seek link

--- theme/header.php
+++ theme/header.php
@@ -53,7 +53,10 @@ $teach_page = is_category('teach') ||
       });
     });
   </script>
-  <script type="text/javascript" src="http://www.google-analytics.com/ga.js"></script>
+ <script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
   <script type="text/javascript">
     try { 
 	var pageTracker=_gat._getTracker("UA-7441223-1");
