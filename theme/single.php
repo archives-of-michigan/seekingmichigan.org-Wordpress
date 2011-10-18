f3c6d9d18fa9d6e8a3b6c755e447b8cbb3e2cd13
Jill Arnold <arnoldj1@michigan.gov>
2011-10-17 22:23:44 -0400

    Discovered switching code in single.php; edited to call correct single templates for new Teach pages

--- theme/single.php
+++ theme/single.php
@@ -6,13 +6,26 @@ case 'look':
     include('single_look.php');
     break;
 case 'teach':
-    include('single_teach.php');
+	switch($path[1]) {
+	case 'events':
+		include('single_events.php');
+		break;
+	case 'programs':
+		include('single_programs.php');
+		break;
+	case 'lessons':
+		include('single_lessons.php');
+		break;
+	
+	default:
+		include('single_teach.php');
+	}
     break;
 case 'civil-war':
     include('single_civil_war.php');
     break;
 case 'events':
-    include('single_events.php');
+    include('single_civil_war.php');
     break;
  
 default:
