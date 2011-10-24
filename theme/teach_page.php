2ae5b70a0de4c9dddd83d9aa81f94167113f1576
Jill Arnold <arnoldj1@michigan.gov>
2011-10-24 13:46:55 -0400

    Updating "Teach Page" template for pages

--- theme/teach_page.php
+++ theme/teach_page.php
@@ -11,25 +11,18 @@ while (have_posts()) {
 
 $js_includes = array('http://s7.addthis.com/js/152/addthis_widget.js');
 $breadcrumbs = array('Teach' => '/teach', $title => '');
-define('BODY_CLASS','teach landing sub page');
+define('BODY_CLASS','teach landing');
+
 include('header.php');
 ?>
-<div id="lead-in">
-  <p>Ideas for teaching kids to Seek, Discover, and Look at Michigan’s Stories.</p>
-  <div class="search">
-    <form id="teach-search" action="http://seekingmichigan.org" method="get" >
-      <label for="search-top" class="hidden">Seek: </label>
-      <input type="text" name="s" id="s" placeholder="Enter text " />
-      <label for="search-button" class="hidden">Search </label>
-      <input type="submit" value=" " id="search-button" name="search-button" />
-    </form>
-  </div>
+<div id="main-content">
+	<div id="left-main-content">
+	<?= $content; ?>
+	</div>
+	<?= app()->partial('sidebar_teach_events'); ?>
 </div>
 
-<div id="main-content">
-  <div class="viewer">
-    <h2><?= $title; ?></h2>
-    <?= $content; ?>
-  </div>
-</div>
+<div id="main-whitebox-left"></div>
+<div id="main-whitebox-right"></div>
+
 <? include('footer.php'); ?>
