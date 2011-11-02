469c50ef555df01533c4d0a5f23dcc03ac23a0b0
Jill Arnold <arnoldj1@michigan.gov>
2011-11-01 23:08:35 -0400

    Trying to get the stand-in image for pdf download to work

--- theme/single_programs.php
+++ theme/single_programs.php
@@ -23,7 +23,7 @@ include('header.php'); ?>
 			       			<h2 id="post-<?= the_ID(); ?>"><a href="<? the_permalink(); ?>" rel="bookmark" title="Permanent Link to <? the_title(); ?>"><? the_title() ?></a></h2>
 			       		</div>
 			       		<div class="pdf_download">
-						<a href="#"><img class="pdf" src="../images/icon-meta-tag-large.gif" />Download This</a>
+						<a href="#"><img class="pdf" src="/images/icon-meta-tag-large.gif" />Download This</a>
 					</div>
 				</div>
 				<?= the_content(); ?>
