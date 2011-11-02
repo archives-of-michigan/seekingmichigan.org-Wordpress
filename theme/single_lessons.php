51c458977909bb2595dfd0d69c918b51192c61ec
Jill Arnold <arnoldj1@michigan.gov>
2011-11-02 15:33:57 -0400

    Added php to make pdf custom field work.

--- theme/single_lessons.php
+++ theme/single_lessons.php
@@ -22,9 +22,13 @@ include('header.php'); ?>
 			       		<div class="post-header-title">
 			       			<h2 id="post-<?= the_ID(); ?>"><a href="<? the_permalink(); ?>" rel="bookmark" title="Permanent Link to <? the_title(); ?>"><? the_title() ?></a></h2>
 			       		</div>
+			       		
+			       		<?php if( get_post_meta($post->, 'pdf', true) : ?>	
 			       		<div class="pdf_download">
-						<a href="#"><img class="pdf" src="../images/icon-meta-tag-large.gif" />Download This</a>
+						<a href="<?php echo get_post_meta($post->ID, 'pdf', true); ?>"><img class="pdf" src="../images/icon-meta-tag-large.gif" />Download This</a>
 					</div>
+					<?php endif; ?>
+					
 				</div>
 				<?= the_content(); ?>
 				<div class="post-meta">
