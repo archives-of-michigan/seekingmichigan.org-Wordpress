39bd2f0060d1fa724962bff7833c788782a7c699
Jill Arnold <arnoldj1@michigan.gov>
2011-11-08 16:39:22 -0500

    Additional / for the url to the tag page

--- theme/include/partials/sidebar_teach_lessons.php
+++ theme/include/partials/sidebar_teach_lessons.php
@@ -15,7 +15,7 @@
 		<h2>Find Lessons by Theme:</h2>
 		<ul>
 			<li><a href="http://seekingmichigan.org/coming-soon">Statehood</a></li>
-			<li><a href="/pioneer-life">Pioneer Life</a></li>
+			<li><a href="/pioneer-life/">Pioneer Life</a></li>
 			<li><a href="http://seekingmichigan.org/coming-soon">Civil War</a></li>
 		</ul>
 		<h2>Find Lessons by Age:</h2>
