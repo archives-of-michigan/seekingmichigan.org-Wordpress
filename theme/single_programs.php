a9272f7299d4595ffed36ff6c9ed4a89d1942c7c
Jill Arnold <arnoldj1@michigan.gov>
2011-12-15 16:09:00 -0500

    Trying out the application custom field.

--- theme/single_programs.php
+++ theme/single_programs.php
@@ -27,6 +27,8 @@ include('header.php'); ?>
 					<li class="share-link"><a class="addthis" href="http://www.addthis.com/bookmark.php" rel="" title="">Share This</a></li>
 					<?php if( get_post_meta($post->ID, 'registration', true)) : ?>
 						<li class="registration"><a href="<?php echo get_post_meta($post->ID, 'registration', true); ?>"><span class="hidden">Register for This</span></a></li>
+					<?php if( get_post_meta($post->ID, 'application', true)) : ?>
+						<li class="application"><a href="<?php echo get_post_meta($post->ID, 'application', true); ?>"><span class="hidden">Apply for This</span></a></li>
 					<?php endif; ?>
 					</ul>
 				</div>
